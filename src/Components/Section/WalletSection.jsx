3303b3a08cf87844cb095490f390bd8ce6afe5ef
mahfuj890 <mahfujali490@gmail.com>
2022-09-15 16:46:57 +0600

    add counter

--- src/Components/Section/WalletSection.jsx
+++ src/Components/Section/WalletSection.jsx
@@ -1,16 +1,7 @@
-import React, { PureComponent } from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { LineChart, Line, Tooltip, ResponsiveContainer } from "recharts";
 import PageTitle from "../PageTitle";
-
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 const dataEarning = [
   {
     name: "Point A",
@@ -49,8 +40,34 @@ function WalletSection() {
       <div className="my_wallet_item border_linear">
         <div className="wallet_title_area">
           <h5>Your Balance</h5>
-          <h4>
-            ETH <span>21.533.10</span>
+          <h4
+            style={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "10px",
+            }}
+          >
+            ETH
+            <VisibilitySensor partialVisibility offset={{ top: 100 }}>
+              {({ isVisible }) => (
+                <div
+                  style={{ height: "100px", lineHeight: "100px", width: "45%" }}
+                >
+                  {isVisible ? (
+                    <CountUp
+                      start={0}
+                      end={21.5331}
+                      duration={2.75}
+                      separator=" "
+                      decimals={4}
+                      decimal="."
+                    />
+                  ) : null}
+                </div>
+              )}
+            </VisibilitySensor>
           </h4>
         </div>
         <div className="text-center">
@@ -92,6 +109,56 @@ function WalletSection() {
                     dot={{
                       stroke: "rgba(13, 219, 132, 0.5)",
                       fill: "#0DDB84",
+                      strokeWidth: 2,
+                    }}
+                    activeDot={{ r: 5 }}
+                  />
+
+                  <Tooltip
+                    contentStyle={{
+                      backgroundColor: "#131313",
+                      border: "1px solid #131313",
+                    }}
+                    labelStyle={{ color: "white" }}
+                    cursor={false}
+                    labelFormatter={(name) => "Number: " + name}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+          <div className="wallet_chart_item">
+            <div className="chart_title_area">
+              <div className="chart_icon">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M16.0914 16.0924V0.907227H12.2951V16.0924H16.0914ZM10.397 16.0924V5.6526H6.60069V16.0924H10.397ZM4.70255 16.0924V8.49982H0.90625V16.0924H4.70255Z"
+                    fill="#FF0000"
+                  />
+                </svg>
+              </div>
+              <h5>Spendings</h5>
+            </div>
+            <h4>
+              2.013 <span>ETH</span>
+            </h4>
+            <div className="wallet_chart">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart width={300} height={100} data={dataEarning}>
+                  <Line
+                    type="monotone"
+                    dataKey="pv"
+                    stroke="#FF0000"
+                    strokeWidth={2}
+                    dot={{
+                      stroke: "rgba(255, 0, 0, 0.581)",
+                      fill: "#FF0000",
                       strokeWidth: 2,
                     }}
                     activeDot={{ r: 5 }}
